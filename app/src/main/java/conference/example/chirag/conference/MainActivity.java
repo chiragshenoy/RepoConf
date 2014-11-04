91a1675f9de20326edab669ae0c6b7c344b1ce4e
chiragshenoy <chiragshenoy@gmail.com>
2014-11-04 21:51:57 +0530

    Merge branch 'master' of https://github.com/chiragshenoy/RepoConf
    Conflicts:
    	app/src/main/java/conference/example/chirag/conference/MainActivity.java

--- app/src/main/java/conference/example/chirag/conference/MainActivity.java
+++ app/src/main/java/conference/example/chirag/conference/MainActivity.java
@@ -1,7 +1,6 @@
 package conference.example.chirag.conference;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,10 +8,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 
 //Pushed from laptop
+//Now pushed from desktop
 public class MainActivity extends Activity {
 
     @Override
